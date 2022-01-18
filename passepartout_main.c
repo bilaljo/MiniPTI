4b2522df3d004ddfc42f9397a86bef6fa6e4d629
Jonas Bilal <jonasbilal@outlook.com>
2022-01-18 16:01:07 +0100

    main file propperly renamed

--- passepartout_main.c
+++ passepartout_main.c
@@ -3,11 +3,21 @@
 #include <stdio.h>
 
 int main(void) {
-  char names[NUMBER_OF_ROWS][NAME_SIZE] = {{0}};
-  size_t number_of_columns = get_names("data.csv", names);
+  struct csv_file csv_file = {
+    .names = {{0}},
+    .data = {{0}},
+  };
+  size_t number_of_columns = get_names("data.csv", csv_file.names);
   for (int i = 0; i < number_of_columns - 1; i++) {
-    printf("%s,", names[i]);
+    printf("%s,", csv_file.names[i]);
   }
-  printf("%s", names[number_of_columns - 1]);
+  printf("%s", csv_file.names[number_of_columns - 1]);
+  get_data("data.csv", csv_file.data, number_of_columns);
+  for (size_t i = 0; i < NUMBER_OF_COLUMNS; i++) {
+    for (size_t j = 0; j < number_of_columns; j++) {
+      printf("%e,", csv_file.data[i][j]);
+    }
+    printf("%e\n", csv_file.data[i][number_of_columns - 1]);
+  }
   return 0;
 }
