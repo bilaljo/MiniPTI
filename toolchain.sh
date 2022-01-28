b5fbed8e93b3029ca5150f5cdc25f36e3fe519cc
Jonas Bilal <jonasbilal@outlook.com>
2022-01-28 21:37:21 +0100

    Script for toolchain finshied

--- toolchain.sh
+++ toolchain.sh
@@ -4,51 +4,66 @@ BINUTILS="https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.gz"
 GCC="https://ftp.gnu.org/gnu/gcc/gcc-6.4.0/gcc-6.4.0.tar.gz"
 GDB="https://ftp.gnu.org/gnu/gdb/gdb-11.2.tar.gz"
 NEW_LIB="https://sourceware.org/pub/newlib/newlib-4.2.0.20211231.tar.gz"
+GSL="https://mirror.ibcp.fr/pub/gnu/gsl/gsl-2.7.1.tar.gz"
 
 BINUTILS_ARCHIV=$(echo $BINUTILS | cut -d '/' -f 6)
 GCC_ARCHIV=$(echo $GCC | cut -d '/' -f 7)
 GDB_ARCHIV=$(echo $GDB | cut -d '/' -f 6)
-NEW_LIB_ARCHIV=$(echo $NEW_LIB | cut -d '/' -f 6)
+NEW_LIB_ARCHIV=$(echo $NEW_LIB | cut -d '/' -f 5)
+GSL_ARCHIV=$(echo $GSL | cut -d '/' -f 7)
 
-TARGET=arm-none-eabi
-PREFIX=/home/$whoami/arm-none-abi
+TARGET="arm-none-eabi"
+PREFIX="/home/($whoami)/arm-none-abi"
 CPU=cortex-m4
-ARM_FLAGS=--with-no-thumb-interwork --with-mode=thumb
-CONFIGURE_BINUTILS=--target=$TARGET --prefix=$PREFIX --with-cpu=$CPU $ARM_FLAGS
+ARM_FLAGS="--with-no-thumb-interwork --with-mode=thumb"
+CONFIGURE_BINUTILS="--target=$TARGET --prefix=$PREFIX --with-cpu=$CPU $ARM_FLAGS"
 
-#wget $BINUTILS $GCC $GDB
+wget $BINUTILS $GCC $GDB $NEW_LIB $GSL
 
 tar -xvzf $BINUTILS_ARCHIV
 tar -xvzf $GCC_ARCHIV
 tar -xvzf $GDB_ARCHIV
 tar -xvzf $NEW_LIB_ARCHIV
+tar -xvzf $GSL_ARCHIV
 
-sudo mkdir -p ~/toolchain/build/binutils-build
+# Cross compile binutils
+mkdir -p ~/toolchain/build/binutils-build
+echo $pwd
 cd ~/toolchain/build/binutils-build
-~/$(echo $BINUTILS_ARCHIV | cut -d ".tar" -f 1)/configure $CONFIGURE_BINUTILS
-make all install 2>&1 | tee ~/toolchain/build/binutils-build/binutils-build-logs.log
+~/$(basename $BINUTILS_ARCHIV .tar.gz)/configure $CONFIGURE_BINUTILS
+make all install 2>&1 | tee ./binutils-build-logs.log
 
-export PATH="$PATH:/home/$whoami/arm-none-eabi/bin"
+export PATH="$PATH:/home/($whoami)/arm-none-eabi/bin"
 
-sudo mkdir -p ~/toolchain/build/gcc-build
+# Cross compile gcc
+mkdir -p ~/toolchain/build/gcc-build
 cd ~/toolchain/build/gcc-build
-~/$(echo $GCC_ARCHIV | cut -d ".tar" -f 1)/configure --target=$TARGET --prefix=$PREFIX \
+~/"$(basename $GCC_ARCHIV .tar.gz)"/configure --target=$TARGET --prefix=$PREFIX \
 --enable-languages=c,c++ --without-headers --with-newlib $ARM_FLAGS
-make all-gcc install-gcc 2>&1 | tee ./gcc-build-withoutnewlib-logs.log
+make all-gcc install-gcc 2>&1 | tee ./gcc-build-logs.log
 
-sudo mkdir -p ~/toolchain/build/newlib-build
+# Cross compile newlib
+mkdir -p ~/toolchain/build/newlib-build
 cd ~/toolchain/build/newlib-build
-~/$(echo $NEW_LIB_BUILD | cut -d ".tar" -f 1)/configure --target=$TARGET --prefix=$PREFIX \
+~/$(basename $NEW_LIB_ARCHIV .tar.gz)/configure --target=$TARGET --prefix=$PREFIX \
 --disable-newlib-supplied-syscalls
 make all install 2>&1 | tee ./newlib-build-logs.log
 
-sudo mkdir ~/toolchain/build/gcc-build
+# Cross compile gcc again with newlib
+mkdir ~/toolchain/build/gcc-build
 cd ~/toolchain/build/gcc-build
-~/$(echo $GCC_ARCHIV | cut -d ".tar" -f 1)/configure --target=$TARGET --prefix=$PREFIX --with-cpu=$CPU \
+~/$(basename $GCC_ARCHIV .tar.gz)/configure --target=$TARGET --prefix=$PREFIX --with-cpu=$CPU \
 --enable-languages=c,c++ --with-newlib $ARM_FLAGS
-make all-gcc install-gcc 2>&1 | tee ./gcc-build-withnewlib-logs.log
+make all-gcc install-gcc 2>&1 | tee ./gcc-build-logs.log
 
-sudo mkdir ~/toolchain/build/gdb-build
+# Cross compile gdb
+mkdir ~/toolchain/build/gdb-build
 cd ~/toolchain/build/gdb-build
-~/$(echo $GDB_ARCHIV | cut -d ".tar" -f 1)/configure --target=$TARGET --prefix=$PREFIX
+~/$(basename $GDB_ARCHIV .tar.gz)/configure --target=$TARGET --prefix=$PREFIX
 make all install 2>&1 | tee ./gdb-build-logs.log
+
+# Cross compile the gsl
+mkdir ~/toolchain/build/gsl-build
+cd ~/toolchain/build/gsl-build
+~/$(basename $GSL_ARCHIV .tar.gz)/configure --target=$TARGET --prefix=$PREFIX $ARM_FLAGS
+make all install 2>&1 | tee ./gsl-build-logs.log
