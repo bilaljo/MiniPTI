eb0db375226d31eb61b8abda1fe9a031afbe928a
Jonas Bilal <jonasbilal@outlook.com>
2022-02-18 22:22:42 +0100

    Reordered phase scan in its own directory

---
+++ src/phase_scan/passepartout_main.c
@@ -0,0 +1,29 @@
+#include <string.h>
+#include <stdlib.h>
+#include "read_csv.h"
+#include "system_phases.h"
+
+int main(int argc, char **argv) {
+  struct csv csv_file;
+  char file_path[BUFFER_SIZE] = "../../dc.csv";
+  if (argc > 1) {
+    strcpy(file_path, argv[1]);
+  }
+  if (! read_csv(file_path, &csv_file)) {
+    exit(1);
+  }
+  double correction_phases[2] = {0};
+  scale_signal(get_column(&csv_file, "Detector 1"));
+  scale_signal(get_column(&csv_file, "Detector 2"));
+  scale_signal(get_column(&csv_file, "Detector 3"));
+  struct intensities intensities = {
+    .detector_1 = get_column(&csv_file, "Detector 1"),
+    .detector_2 = get_column(&csv_file, "Detector 2"),
+    .detector_3 = get_column(&csv_file, "Detector 3"),
+  };
+  get_phases(correction_phases, &intensities);
+  if (! save_data("phases.csv", &correction_phases)) {
+    return 1;
+  }
+  return 0;
+}
