a51ce86f49fecb62d0bcaea605a968d258724050
bilalj <jonasbilal@outlook.com>
2022-04-12 18:21:49 +0200

    exe file generator for windows

---
+++ gui/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+import py2exe, sys, os
+
+entry_point = sys.argv[1]
+sys.argv.pop()
+sys.argv.append('py2exe')
+sys.argv.append('-q')
+
+opts = {
+    'py2exe': {
+        'compressed': 0,
+        'optimize': 2,
+        'bundle_files': 1,
+    }
+}
+
+setup(windows = [{'script': "__main__.py"}], options=opts, zipfile="main.zip")
