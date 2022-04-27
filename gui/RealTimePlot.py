a741dea29efba899b5f2ad4ed7d06e5487c28972
bilaljo <jonasbilal@outlook.com>
2022-04-27 22:50:19 +0200

    WIP: Real Time Plotting

--- gui/RealTimePlot.py
+++ gui/RealTimePlot.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from collections import deque
 import matplotlib.animation as animation
 import matplotlib.dates as mdates
+from datetime import date
 
 
 class RealTimePlot(Plotting):
@@ -17,6 +18,8 @@ class RealTimePlot(Plotting):
         self.pti = deque(maxlen=intervall)  # With this we limit the maxium drawn points in plot.
         self.phase = deque(maxlen=intervall)
         self.data = None
+        self.pti_data = None
+        self.phase_data = None
 
     @staticmethod
     def __get_data_generator():
@@ -32,13 +35,17 @@ class RealTimePlot(Plotting):
     def set_csv(self):
         self.data = self.__get_data_generator()
 
+    def set_data(self):
+        self.pti_data, self.phase_data = next(self.data)
+
     def animate_pti(self, i):
+        self.set_data()
         self.axes["PTI Signal"].clear()
-        xfmt = mdates.DateFormatter('%d-%m-%y %H:%M')
+        xfmt = mdates.DateFormatter("%d-%m-%y %H:%M")
         self.axes["PTI Signal"].xaxis.set_major_formatter(xfmt)
         self.axes["PTI Signal"].autofmt_xdate()
-        self.time.append()
-        self.pti.append(self.data[0])
+        self.time.append(date.today().strftime("%d-%m-%y %H:%M"))
+        self.pti.append(self.pti_data)
         self.axes["PTI Signal"].plot(self.time, self.pti)
         self.axes["PTI Signal"].set_xlabel("Time in s", fontsize=12)
         self.axes["PTI Signal"].set_ylabel("PTI Signal in rad", fontsize=12)
@@ -49,7 +56,7 @@ class RealTimePlot(Plotting):
         xfmt = mdates.DateFormatter('%d-%m-%y %H:%M:%s')
         self.axes["Interferometric Phase"].xaxis.set_major_formatter(xfmt)
         self.axes["Interferometric Phase"].autofmt_xdate()
-        self.pti.append(self.data[1])
+        self.pti.append(self.phase_data)
         self.axes["Interferometric Phase"].plot(self.time, self.phase)
         self.axes["Interferometric Phase"].set_xlabel("Time in s", fontsize=12)
         self.axes["Interferometric Phase"].set_ylabel("PTI Signal in rad", fontsize=12)
@@ -62,3 +69,8 @@ class RealTimePlot(Plotting):
     def live_plot(self):
         animation.FuncAnimation(self.figures["PTI Signal"], self.animate_pti, interval=1000, frames=1)
         animation.FuncAnimation(self.figures["Interferometric Phase"], self.animate_phase, interval=1000, frames=1)
+        plt.show()
+
+    def execute(self):
+        self.live_plot()
+        self.main_window.after()
