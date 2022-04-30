6490783d60210c3b8e58edc3b71daaf41fe40701
bilaljo <jonasbilal@outlook.com>
2022-04-30 14:22:13 +0200

    Implemented the decimation in python

--- src/PTI_Python/Decimation.py
+++ src/PTI_Python/Decimation.py
@@ -1,74 +1,75 @@
-from scipy import signal as sig
 import numpy as np
 
 
-
 class Decimation:
+    """
+    Provides the methods for an software based Lock-In-Amplififer and decimation.
+
+    This class parses binary data with a definined encoding: the first 3 x 50,000 Doubles
+    represent DC-coupled signals. The next 50.000 Doubles represent the measured reference
+    signal. The last 3 x 50,000 Doubles represent the measured AC-coupled signals.
+    The reference for the Lock-In-Amplifier is to assumed to have a stable frequency at
+    80 Hz.
+    The sample frequency is at 50 kHz. With 1 s decimation intervall this results in
+    50,000 samples.
+
+    Attributes:
+        samples: int
+            The number of samples.
+        dc: np.array
+            The DC-coupled signals for each channel.
+        ac: np.array
+            The AC-coupled signals for each channel.
+        dc_down_sampled: np.array
+            The decimated DC-coupled signals for each channel.j
+        in_phase: np.array
+            The sine-reference for the Lock-In-Amplifier.
+        quadratur: np.array
+            The cosine-reference for the Lock-In-Amplifier.
+        ac_x: np.array
+            The In-Phase-Component for each Detector after Lock-In-Amplifier.
+        ac_y: np.array
+            The Quadratur-Component for each Detector after Lock-In-Amplifier.
+        file: _io.TextIOWrapper
+            The binary file with the measurements.
+    """
     def __init__(self, file_name):
-        self.dc = []
-        self.ac = []
-        self.dc_down_sampled = []
-        self.ref = None
-        self.file_name = file_name
-        self.first_read = True
         self.samples = 50000
-        self.in_phase = np.sin(np.linspace(0, 2 * np.pi, self.samples) / 624 - 60)
-        self.quadratur = np.cos(np.linspace(0, 2 * np.pi, self.samples) / 624 - 60)
-        self.amplification = 1000
-        self.ac_x = []
-        self.ac_y = []
-        self.file = None
+        self.dc = np.empty(shape=(3, self.samples))
+        self.ac = np.empty(shape=(3, self.samples))
+        self.dc_down_sampled = np.empty(shape=3)
+        time = np.linspace(0, 1, self.samples) + 67.5  # The reference has a time shift.
+        self.in_phase = np.sin(2 * np.pi * time * 80)
+        self.quadratur = np.cos(2 * np.pi * time * 80)
+        self.amplification = 1000  # The amplification is definied by the hardware setup.
+        self.ac_x = np.empty(shape=3)
+        self.ac_y = np.empty(shape=3)
+        self.file = open(file_name, "rb")
 
     def read_data(self):
-        self.ac = []
-        self.dc = []
-        if self.first_read:
-            self.file = open(self.file_name, "rb")
-            # TODO: Obsolet
-            self.file.read(30)
-            self.first_read = False
+        """
+        Reads the binary data and save it into numpy arrays.
+        """
         np.frombuffer(self.file.read(4), dtype=np.intc)
         np.frombuffer(self.file.read(4), dtype=np.intc)
         for channel in range(3):
-            self.dc.append(np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64))
-        self.ref = np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64)
+            self.dc[channel] = np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64)
+        np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64)
         for channel in range(3):
-            self.ac.append(np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64) / self.amplification)
-
-    @staticmethod
-    def low_pass(data, fs, order, fc):
-        nyq = 0.5 * fs  # Calculate the Nyquist frequency.
-        cut = fc / nyq  # Calculate the cutoff frequency (-3 dB).
-        lp_b, lp_a = sig.butter(order, cut, btype='lowpass')  # Design and apply the low-pass filter.
-        lp_data = list(sig.filtfilt(lp_b, lp_a, data))  # Apply forward-backward filter with linear phase.
-        return lp_data
+            self.ac[channel] = np.frombuffer(self.file.read(self.samples * 8), dtype=np.float64) / self.amplification
 
     def calucalte_dc(self):
-        self.dc_down_sampled = []
-        for channel in range(3):
-            dc_down_sampled = self.dc[channel]
-            dc_down_sampled = self.low_pass(dc_down_sampled, fs=50e3, order=2, fc=0.01)
-            for i in range(4):
-                dc_down_sampled = sig.decimate(dc_down_sampled, 10)
-            #dc_down_sampled = sig.decimate(dc_down_sampled, 5)
-            self.dc_down_sampled.append(np.mean(dc_down_sampled))
+        """
+        Applies a low pass to the DC-coupled signals and decimate it to 1 s values.
+        """
+        self.dc_down_sampled = np.mean(self.dc, axis=1)
 
     def common_mode_noise_reduction(self):
-        total_dc = sum(self.dc)
-        noise = sum(self.ac)
+        total_dc = sum(self.dc_down_sampled)
+        noise = np.sum(self.ac, axis=0)
         for channel in range(3):
-            self.ac[channel] = self.ac[channel] - self.dc[channel] / total_dc * noise
+            self.ac[channel] = self.ac[channel] - self.dc_down_sampled[channel] * noise / total_dc
 
     def lock_in_amplifier(self):
-        self.ac_x = []
-        self.ac_y = []
-        for channel in range(3):
-            in_phase = self.ac[channel] * self.in_phase
-            quadratur = self.ac[channel] * self.quadratur
-            in_phase_down_sampled = self.low_pass(data=in_phase, fs=50e3, order=2, fc=1)
-            quadratur_down_sampled = self.low_pass(data=quadratur, fs=50e3, order=2, fc=1)
-            for i in range(4):
-                in_phase_down_sampled = sig.decimate(in_phase_down_sampled, 10)
-                quadratur_down_sampled = sig.decimate(quadratur_down_sampled, 10)
-            self.ac_x.append(np.mean(in_phase_down_sampled))
-            self.ac_y.append(np.mean(quadratur_down_sampled))
+        np.mean(self.ac * self.in_phase, axis=1, out=self.ac_x)
+        np.mean(self.ac * self.quadratur, axis=1, out=self.ac_y)
---
+++ src/PTI_Python/decimate.py
@@ -0,0 +1,44 @@
+import time
+import pandas as pd
+import os
+import logging
+from Decimation import Decimation
+
+
+def decimate(file, outputfile):
+    """
+    Applies the Noise-Reduction Algorithm, Decimation and Lock-In-Amplifier on binary data of given
+    filename.
+
+    Every iteration writes the results
+
+    :param file: str
+        The file name of the binary raw data.
+    :param outputfile: str
+        The filename of the output file for the results.
+    :return: None
+    """
+    # start = time.process_time()
+    decimation = Decimation(file_name=file)
+    if decimation.file is None:
+        logging.error("The file does not exist.")
+        return
+    if os.path.exists(outputfile):
+        os.remove(outputfile)
+    elements = 1800
+    pd.DataFrame()
+    for i in range(elements):
+        decimation.read_data()
+        decimation.calucalte_dc()
+        decimation.common_mode_noise_reduction()
+        decimation.lock_in_amplifier()
+        dc = decimation.dc_down_sampled
+        x = decimation.ac_x
+        y = decimation.ac_y
+        pd.DataFrame({"X1": x[0], "X2": x[0], "X3": x[2],
+                      "Y1": y[0], "Y2": x[0], "Y3": y[2],
+                      "DC1": dc[0], "DC2": dc[1], "DC3": dc[2]},
+                     index=[0]
+                     ).to_csv(outputfile, mode="a", header=not os.path.exists(outputfile))
+    decimation.file.close()
+    # print(time.process_time() - start)
