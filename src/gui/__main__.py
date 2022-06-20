594b4207dde3f01d81e710e324ecbbadefc88ccc
bilaljo <jonasbilal@outlook.com>
2022-06-20 09:14:12 +0200

    Refactoring

--- src/gui/__main__.py
+++ src/gui/__main__.py
@@ -1,33 +1,41 @@
 import tkinter as tk
 from tkinter import ttk
 
-from mode import Mode
 
+class MainWindow(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__()
+        self.mode_frame = None
+        self.offline = None
+        self.live = tk.IntVar()
+        self.live_button = None
+        self.offline = self.live
+        self.offline_button = None
+        self.theme = "light"
+        self.parent = parent
+        self.setup()
 
-class Theme:
-    theme = "dark"
+    def setup(self):
+        self.mode_frame = ttk.LabelFrame(self, text="Mode", padding=(20, 10))
+        self.mode_frame.grid(row=2, column=0, padx=(20, 10), pady=10, sticky="nsew")
 
-    root = None
+        self.offline_button = ttk.Radiobutton(self.mode_frame, text="Offline", variable=self.offline, value=1)
+        self.offline_button.grid(row=0, column=0, padx=5, pady=10, sticky="nsew")
+        self.live_button = ttk.Radiobutton(self.mode_frame, text="Live", variable=self.live, value=2)
+        self.live_button.grid(row=1, column=0, padx=5, pady=10, sticky="nsew")
 
-    @staticmethod
-    def set_theme(event=None):
-        Theme.theme = "dark" if Theme.theme == "light" else "light"
-        Theme.root.tk.call("set_theme", Theme.theme)
+    def set_theme(self, event):
+        self.theme = "dark" if self.theme == "light" else "light"
+        self.parent.tk.call("set_theme", self.theme)
 
 
 def main():
     root = tk.Tk()
-    #Theme.root = root
     root.tk.call("source", "azure.tcl")
     root.tk.call("set_theme", "light")
-    #root.bind("<F1>", Theme.set_theme)
-
-    mode_frame = ttk.LabelFrame(root, text="Mode", padding=(20, 10))
-    mode_frame.grid(row=2, column=0, padx=(20, 10), pady=10, sticky="nsew")
-    live_button = tk.Radiobutton(mode_frame, text="Live", command="", value=1)
-    offline_button = tk.Radiobutton(mode_frame, text="Offline", command="", value=2)
-    live_button.grid(row=2, column=0, sticky="nsew")
-    offline_button.grid(row=3, column=0)
+    main_window = MainWindow(root)
+    main_window.pack(fill="both", expand=True)
+    root.bind("<F1>", main_window.set_theme)
     root.mainloop()
 
 
--- src/gui/example.py
+++ src/gui/example.py
@@ -1,10 +1,31 @@
+"""
+Example script for testing the Azure ttk theme
+Author: rdbende
+License: MIT license
+Source: https://github.com/rdbende/ttk-widget-factory
+"""
+
+
 import tkinter as tk
 from tkinter import ttk
 
 
-class App:
+class App(ttk.frame):
     def __init__(self, parent):
         self.root = parent
+
+        # Create widgets :)
+        self.setup_widgets()
+
+    def setup_widgets(self):
+        # Create a Frame for the Radiobuttons
+        self.radio_frame = ttk.LabelFrame(self.root, text="Mode", padding=(20, 10))
+        self.radio_frame.grid(row=2, column=0, padx=(20, 10), pady=10, sticky="nsew")
+
+        # Radiobuttons
+        self.radio_1 = ttk.Radiobutton(
+            self.radio_frame, text="Unselected", variable=None, value=1)
+        self.radio_1.grid(row=0, column=0, padx=5, pady=10, sticky="nsew")
 
 if __name__ == "__main__":
     root = tk.Tk()
