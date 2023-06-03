a4d130d5b54ad340f0e87af649f588cebde38e13
bilaljo <jonasbilal@outlook.com>
2023-06-03 11:39:13 +0200

    WIP: Refactoring of Tec

--- minipti/hardware/commands.py
+++ minipti/hardware/commands.py
@@ -1,6 +1,20 @@
 from typing import NamedTuple
 
 
+class SerialValue:
+    _BYTE_PREFIX = 3
+
+    def __int__(self):
+        self.value: str = ""
+
+    def __ror__(self, other):
+        if isinstance(other, str):
+            self.value[SerialValue._BYTE_PREFIX:] = other
+        else:
+            self.value[SerialValue._BYTE_PREFIX:] = other.value
+
+
 class Tec(NamedTuple):
     set_p_value: str = "SP0000"
     set_i_1_value: str = "SI0000"
+    
