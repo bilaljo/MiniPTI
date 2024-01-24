d6ebf9f043aefd39a836bc9e7eb506a179e9fab6
bilaljo <jonasbilal@outlook.com>
2024-01-24 17:45:06 +0100

    Example fixed

--- examples/pti_inversion.py
+++ examples/pti_inversion.py
@@ -11,21 +11,26 @@ from minipti.algorithm import interferometry, pti
 
 
 if __name__ == "__main__":
-    interferometer = interferometry.Interferometer(settings_path="examples/sample_configs/settings.csv")
+    base_path = f"{os.path.dirname(__file__)}"
+    interferometer = interferometry.Interferometer(settings_path=f"{base_path}/sample_configs/settings.csv")
     interferometer.load_settings()
-    interferometer.decimation_filepath = "examples/sample_data/Decimation_Comercial.csv"
-    inversion = pti.Inversion(interferometer=interferometer, settings_path="examples/sample_configs/settings.csv")
+    interferometer.decimation_filepath = f"{base_path}/sample_data/Decimation_Comercial.csv"
+    inversion = pti.Inversion(
+        interferometer=interferometer,
+        settings_path=f"{base_path}/sample_configs/settings.csv"
+    )
     inversion.load_response_phase()
 
     # Using the wrapper for pti inversion
-    inversion.run(file_path="examples/sample_data/Decimation_Comercial.csv")
+    inversion.run(file_path=f"{base_path}/sample_data/Decimation_Comercial.csv")
     print(inversion)
 
     # Using the API
-    data = pd.read_csv("examples/sample_data/Decimation_Comercial.csv")
+    data = pd.read_csv(f"{base_path}/sample_data/Decimation_Comercial.csv")
     interferometer.intensities = data[[f"DC CH{i}" for i in range(1, 4)]].to_numpy()
     inversion.decimation.lock_in.amplitude = data[[f"Lock In Amplitude CH{i}" for i in range(1, 4)]].to_numpy().T
     inversion.decimation.lock_in.phase = data[[f"Lock In Phase CH{i}" for i in range(1, 4)]].to_numpy().T
     inversion.run(live=False)
     print(inversion)
-    os.remove("PTI_Inversion.csv")
+    os.remove("Offline_PTI_Inversion.csv")
+    os.remove("Offline_Interferometer.csv")
